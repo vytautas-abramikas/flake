daa42d22c086c7e0493a5be12965de5121ccf474
Vytautas Abramikas <abramikas@gmail.com>
2024-12-07 17:08:19 +0200

    ..

--- modules/machine.nix
+++ modules/machine.nix
@@ -53,10 +53,11 @@
       prime = {
         intelBusId = "PCI:0:2:0";
         nvidiaBusId = "PCI:1:0:0";
-        offload = {
-          enable = true;
-          enableOffloadCmd = true;
-        };
+        sync.enable = true;
+        # offload = {
+        #   enable = true;
+        #   enableOffloadCmd = true;
+        # };
       };
     };
   };

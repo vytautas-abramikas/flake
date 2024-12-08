fb00ac5c5ac112fcea56597988a2b23ab1eb21a0
Vytautas Abramikas <abramikas@gmail.com>
2024-12-08 02:51:28 +0200

    trying to reenable offload

--- modules/machine.nix
+++ modules/machine.nix
@@ -54,10 +54,10 @@
         intelBusId = "PCI:0:2:0";
         nvidiaBusId = "PCI:1:0:0";
         sync.enable = true;
-        # offload = {
-        #   enable = true;
-        #   enableOffloadCmd = true;
-        # };
+        offload = {
+          enable = true;
+          enableOffloadCmd = true;
+        };
       };
     };
   };

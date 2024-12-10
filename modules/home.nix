f07e4537dd14377eaa4c14248c32bde0d1768000
Vytautas Abramikas <abramikas@gmail.com>
2024-12-10 06:13:39 +0200

    .

--- modules/home.nix
+++ modules/home.nix
@@ -14,17 +14,17 @@
       ];
     };
 
-    xsession.windowManager.i3 = {
-      enable = true;
-      package = pkgs.i3-gaps;
-      config = {
-        modifier = "Mod4";
-        gaps = {
-          inner = 10;
-          outer = 5;
-        };
-      };
-    };
+    # xsession.windowManager.i3 = {
+    #   enable = true;
+    #   package = pkgs.i3-gaps;
+    #   config = {
+    #     modifier = "Mod4";
+    #     gaps = {
+    #       inner = 10;
+    #       outer = 5;
+    #     };
+    #   };
+    # };
 
   };
 }

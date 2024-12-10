5db27648ebc8e99cf4ea1a59a895161d491da052
Vytautas Abramikas <abramikas@gmail.com>
2024-12-10 05:58:49 +0200

    ..

--- modules/home.nix
+++ modules/home.nix
@@ -6,12 +6,25 @@
       stateVersion = "24.11";
       username = "broliux";
       homeDirectory = "/home/broliux";
-    
-      packages = with pkgs; [
-        git nodejs github-desktop vscodium kdePackages.kcalc
-        brave firefox vlc
-        ventoy-full
-      ];
     };
+
+    packages = with pkgs; [
+      git nodejs github-desktop vscodium kdePackages.kcalc
+      brave firefox vlc
+      ventoy-full
+    ];
+
+    xsession.windowManager.i3 = {
+      enable = true;
+      package = pkgs.i3-gaps;
+      config = {
+        modifier = "Mod4";
+        gaps = {
+          inner = 10;
+          outer = 5;
+        };
+      };
+    };
+
   };
 }
